7420ec9ae8487af74e05a0293e48306c6f777d8a
François ROULLAUD <froullaud@positivethinking.tech>
2022-01-14 12:25:24 +0100

    Add project files.

---
+++ SpecFlowTP2/Choix.cs
@@ -0,0 +1,10 @@
+﻿namespace SpecFlowTP2
+{
+    public enum Choix
+    {
+        VoteBlanc = 1,
+        JohanCampion = 2,
+        FrancoisRoullaud = 3,
+        JcDurand = 4
+    }
+}
---
+++ SpecFlowTP2/Vote.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SpecFlowTP2
+{
+    public class Vote
+    {
+        public Choix choix { get; set; }
+    }
+}
